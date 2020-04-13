528f55b14c8ee22694793d736f80d108f7a3a099
missing-user <philipp.jurasic@gmx.de>
2020-04-13 23:15:13 +0200

    fixes first time load issues

--- script.js
+++ script.js
@@ -31,6 +31,36 @@ function saveSession(lastDate) {
   }
 }
 
+function loadLastSession(data) {
+  if (window.localStorage) {
+    dateKey = window.localStorage.getItem('dateKey')
+    currentDate = Object.keys(data.cases)[Object.keys(data.cases).length - 1]
+    oldTime = parseInt(window.localStorage.getItem('lastTime'))
+
+    timeDiff = (Date.now() - oldTime) % 86400000
+    var c = 0,
+      d = 0,
+      r = 0
+
+    if ((Date.now() - oldTime) > 86400000) {
+      c = data.cases[currentDate] - data.cases[dateKey]
+      d = data.deaths[currentDate] - data.deaths[dateKey]
+      r = data.recovered[currentDate] - data.recovered[dateKey]
+    }
+    c += Math.floor(timeDiff / calcInterval(perDay(data.cases)))
+    d += Math.floor(timeDiff / calcInterval(perDay(data.deaths)))
+    r += Math.floor(timeDiff / calcInterval(perDay(data.recovered)))
+
+    console.log('loaded last session data ', dateKey, oldTime);
+
+    document.getElementById('linf').textContent = c
+    document.getElementById('ldead').textContent = d
+    document.getElementById('lreco').textContent = r
+    //saves the last Date being used as a key in the JSON
+    saveSession(currentDate)
+  }
+}
+
 function onLoaded(data) {
   console.log(data)
   if ("country" in data) {
@@ -47,31 +77,6 @@ function onLoaded(data) {
     setInterval(updateValue, calcInterval(perDay(data.deaths)), 'dead')
     setInterval(updateValue, calcInterval(perDay(data.recovered)), 'reco')
 
-
-    if (window.localStorage) {
-      dateKey = window.localStorage.getItem('dateKey')
-      currentDate = Object.keys(data.cases)[Object.keys(data.cases).length - 1]
-      oldTime = parseInt(window.localStorage.getItem('lastTime'))
-
-      timeDiff = (Date.now() - oldTime) % 86400000
-      var c = 0,
-        d = 0,
-        r = 0
-
-      if ((Date.now() - oldTime) > 86400000) {
-        c = data.cases[currentDate] - data.cases[dateKey]
-        d = data.deaths[currentDate] - data.deaths[dateKey]
-        r = data.recovered[currentDate] - data.recovered[dateKey]
-      }
-      c += Math.floor(timeDiff / calcInterval(perDay(data.cases)))
-      d += Math.floor(timeDiff / calcInterval(perDay(data.deaths)))
-      r += Math.floor(timeDiff / calcInterval(perDay(data.recovered)))
-
-      document.getElementById('linf').textContent = c
-      document.getElementById('ldead').textContent = d
-      document.getElementById('lreco').textContent = r
-      //saves the last Date being used as a key in the JSON
-      saveSession(currentDate)
-    }
+    loadLastSession(data)
   }
 }
